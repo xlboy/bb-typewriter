33e288a3b8e531e8753fec9f501402f1117ef5e1
xlboy <984492408@qq.com>
2020-12-23 23:59:07 +0800

    想个文件夹名称想半天，起名纠结患者，靠。vue3的组合式很舒服，部分小场景可以顶替vuex，特此起一个名称为store-composition/store-hooks的文件夹包揽着这个伪式vuex，哈哈，明天再决定其一

--- h5/src/main.ts
+++ h5/src/main.ts
@@ -5,7 +5,7 @@ import store from "./store";
 import { vantPlugins } from "./plugins/vant";
 // import Bmob, { globalPlugins } from "./plugins/global";
 import "./assets/style/reset.scss";
-import "./assets/style/global.less";
+import "./assets/style/global.scss";
 import "./utils/theme";
 
 
